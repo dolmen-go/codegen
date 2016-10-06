4ef8f4e5ca80a6e95fef5d16a77f2955fb4be714
Olivier Mengué <dolmen@cpan.org>
2016-10-07 00:55:54 +0200

    Fix example

--- example_test.go
+++ example_test.go
@@ -2,12 +2,13 @@ package codegen_test
 
 import (
 	"github.com/dolmen-go/codegen"
+	"log"
 	"os"
 )
 
 func ExampleMustParse() {
 	const template = `
-// +build {{tag}}
+{{/**/}}// +build {{tag}}
 package main
 
 import "os"
@@ -19,6 +20,8 @@ func main() {
 
 	tmpl := codegen.MustParse(template)
 	for _, tag := range os.Args[1:] {
-		tmpl.Create("main_"+tag+".go", tag)
+		if err := tmpl.Create("main_"+tag+".go", tag); err != nil {
+			log.Fatal(err)
+		}
 	}
 }
