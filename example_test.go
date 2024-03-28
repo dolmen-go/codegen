02092b81650af30111cf7e11b2aba91ddfee0e6f
Olivier Mengué <dolmen@cpan.org>
2024-03-28 01:59:29 +0100

    Fix copyright year

--- example_test.go
+++ example_test.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2022 Olivier Mengué
+Copyright 2024 Olivier Mengué
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
