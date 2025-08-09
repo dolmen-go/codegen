0181105fd58bb57abcf5e961dcd7ada9d3830884
Olivier Mengué <dolmen@cpan.org>
2025-08-09 03:09:43 +0200

    Use go/ast.IsGenerated
    Use go/ast.IsGenerated (instead of our GeneratedCodeRegexp) to detect
    the generated code tag.
    
    Closes #3.

---
+++ generated_ast.go
@@ -0,0 +1,37 @@
+//go:build go1.21
+// +build go1.21
+
+/*
+Copyright 2025 Olivier Mengué
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package codegen
+
+import (
+	"go/ast"
+	"go/parser"
+	"go/token"
+)
+
+func isGenerated(code []byte) bool {
+	// Code from https://cs.opensource.google/go/go/+/refs/tags/go1.24.5:src/go/ast/issues_test.go;l=127
+	fset := token.NewFileSet()
+	f, _ := parser.ParseFile(fset, "", code, parser.PackageClauseOnly|parser.ParseComments)
+	if f == nil {
+		return false
+	}
+
+	return ast.IsGenerated(f)
+}
